e200af41b1b666f85a807fc65853c260719d81ea
ryandeng1 <ryandeng@mit.edu>
2026-09-17 00:37:09 +0000

    add OpenHands license to str_replace_editor tool

--- src/perfagent/tools/str_replace_editor.py
+++ src/perfagent/tools/str_replace_editor.py
@@ -4,6 +4,32 @@ The tool description is OpenHands' own `TOOL_DESCRIPTION`, copied below from the
 package (see the comment at DESCRIPTION for the version) 
 """
 
+# The following notice applies to the TOOL_DESCRIPTION text copied from
+# OpenHands software-agent-sdk (openhands-tools 1.47.0).
+# Source: https://github.com/OpenHands/software-agent-sdk/blob/v1.47.0/LICENSE
+#
+# MIT License
+#
+# Copyright (c) 2026 OpenHands contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
 import base64
